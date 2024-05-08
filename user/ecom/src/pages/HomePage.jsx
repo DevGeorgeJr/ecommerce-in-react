a81b73dce34f391cf61ce3ca2071575415fd224a
DevGeorge <georgemanlosajr@gmail.com>
2024-05-08 03:07:23 -0400

    Add the view for new arrival and collection

--- user/ecom/src/pages/HomePage.jsx
+++ user/ecom/src/pages/HomePage.jsx
@@ -1,11 +1,15 @@
 import React, { Fragment } from 'react'
 import FeaturedProducts from '../components/home/FeaturedProducts'
 import Categories from '../components/home/Categories'
+import Collection from '../components/home/Collection'
+import NewArrival from '../components/home/NewArrival'
 
 const HomePage = () => {
   return (
     <Fragment>
       <FeaturedProducts />
+      <NewArrival />
+      <Collection />
       <Categories />
     </Fragment>
   )
